0d9314f767e6969298d544e44fb7c34eac9e4778
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-05-10 16:56:39 +0300

    Switch to Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Home: React.FC = () => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Hello!</h1>
+    </main>
+  </>
+);
+
+export default Home;
