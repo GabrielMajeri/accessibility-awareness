47351d06f6af4eccf9c49d93d4354d3f36f0161c
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-05-13 09:52:18 +0300

    Finish assistive technologies page

--- pages/assistive-technologies.tsx
+++ pages/assistive-technologies.tsx
@@ -1,40 +1,58 @@
+import Image from "next/image";
 import PageHeading from "../components/PageHeading";
 
 const AssistiveTechnologies: React.FC = () => (
   <>
-    <header className="m-4">
+    <header className="p-4 bg-gradient-to-b from-white to-gray-200">
       <PageHeading text="Tehnologii asistive" />
-      <p>
+      <p className="sm:mx-auto sm:max-w-md">
         Pe această pagină prezentăm principalele tehnologii care ajută
-        persoanele cu dizabilități să-și desfășșoare mai ușor activitățile în
+        persoanele cu dizabilități să-și desfășoare mai ușor activitățile în
         viața de zi cu zi.
       </p>
     </header>
-    <section>
-      <img src="" alt="Cititor de ecran" />
-      <span>Deficiențe de vedere</span>
-      <p>
-        Cititoarele de ecran sunt programe care citesc cu voce tare textul aflat
-        pe ecran.
-      </p>
-    </section>
-    <section>
-      <img src="" alt="Scaun cu rotile" />
-      <span>Deficiențe motorii</span>
-      <p>
-        Scaunele cu rotile ajută persoanele cu deficiențe motorii să se
-        deplaseze mai ușor.
-      </p>
-    </section>
-    <section>
-      <img src="" alt="Fontul Lexend Deca" />
-      <span>Dislexie</span>
-      <p>
-        Unele fonturi sunt special concepute să fie mai ușor de citit de către
-        persoanele cu dislexie, cum ar fi cel folosit pe acest site.
-      </p>
-    </section>
+    <div className="px-4 pb-4 bg-gray-200">
+      <AssistiveTechnology
+        name="Cititor de ecran"
+        description="Cititoarele de ecran sunt programe care citesc cu voce tare textul aflat pe ecran."
+        disability="Deficiențe de vedere"
+        imageSrc="/assistive-technologies/nvda-logo.png"
+      />
+      <AssistiveTechnology
+        name="Scaun cu rotile"
+        description="Scaunele cu rotile ajută persoanele cu deficiențe motorii să se deplaseze mai ușor."
+        disability="Deficiențe motorii"
+        imageSrc="/assistive-technologies/wheelchair.jpg"
+      />
+      <AssistiveTechnology
+        name="Fontul Lexend Deca"
+        description="Unele fonturi sunt special concepute să fie mai ușor de citit de către persoanele cu dislexie, cum ar fi cel folosit pe acest site."
+        disability="Dislexie"
+        imageSrc="/assistive-technologies/lexend-deca-specimen.png"
+      />
+    </div>
   </>
+);
+
+const AssistiveTechnology: React.FC<{
+  name: string;
+  description: string;
+  disability: string;
+  imageSrc: string;
+}> = ({ name, description, disability, imageSrc }) => (
+  <section className="mb-8 rounded-lg shadow bg-white sm:mx-auto sm:max-w-sm">
+    <div className="p-4">
+      <div className="relative h-32">
+        <Image src={imageSrc} alt={name} layout="fill" objectFit="contain" />
+      </div>
+    </div>
+    <div className="m-2 text-center">
+      <span className="px-2 py-0.5 rounded-full bg-yellow-400">
+        Pentru: {disability}
+      </span>
+    </div>
+    <p className="p-4">{description}</p>
+  </section>
 );
 
 export default AssistiveTechnologies;
